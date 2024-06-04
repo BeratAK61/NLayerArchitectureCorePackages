04592f44a4d07f4b650167692a413363b7e48501
Berat Ak <brt_ak_61@hotmail.com>
2024-06-04 21:39:30 +0300

    Add Data Result Types

---
+++ MyCorePackages/Results/Concrete/ErrorDataResult.cs
@@ -0,0 +1,25 @@
+﻿namespace MyCorePackages.Results.Concrete;
+
+public class ErrorDataResult<T> : DataResult<T>
+{
+    public ErrorDataResult() : base(default, true)
+    {
+
+    }
+
+    public ErrorDataResult(string message) : base(default, false, message)
+    {
+        
+    }
+
+    public ErrorDataResult(T data) : base(data, false)
+    {
+
+    }
+
+
+    public ErrorDataResult(T data, string message) : base(data, false, message)
+    {
+
+    }
+}
