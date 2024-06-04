99ef1775e0ac79856aa01660b601e5e77b1cd404
Berat Ak <brt_ak_61@hotmail.com>
2024-06-04 21:31:05 +0300

    Add Success Result and Error Result Types

---
+++ MyCorePackages/Results/Concrete/Result.cs
@@ -0,0 +1,20 @@
+﻿using MyCorePackages.Results.Abstract;
+
+namespace MyCorePackages.Results.Concrete;
+
+public class Result : IResult
+{
+    public Result(bool success, string message) : this(success)
+    {
+        Message = message;
+    }
+
+    public Result(bool success)
+    {
+        Success = success;
+    }
+
+    public bool Success { get; }
+
+    public string Message { get; }
+}
