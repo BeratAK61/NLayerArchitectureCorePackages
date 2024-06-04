10e3dfb9c68efcdae42d57475b781da030300978
Berat Ak <brt_ak_61@hotmail.com>
2024-06-05 00:56:26 +0300

    Update Repositories

---
+++ NLayerCore/Repositories/IQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace NLayerCore.Repositories;
+
+public interface IQuery<T>
+{
+    IQueryable<T> Query();
+}
--- NLayerCore/Repositories/IRepository.cs
+++ NLayerCore/Repositories/IRepository.cs
@@ -1,12 +1,47 @@
-﻿using System.Linq.Expressions;
+﻿using Microsoft.EntityFrameworkCore.Query;
+using NLayerCore.Dynamic;
+using NLayerCore.Paging;
+using NLayerCore.Repositories;
+using System.Linq.Expressions;
+
 
 namespace MyCorePackages.Repositories;
 
-public interface IRepository<TEntity> where TEntity : BaseEntity, new()
+public interface IRepository<TEntity, TEntityId> : IQuery<TEntity>
+    where TEntity : Entity<TEntityId>
 {
-    void Delete(TEntity entity);
-    int Create(TEntity entity);
-    void Update(TEntity entity);
-    TEntity Get(Expression<Func<TEntity, bool>> filter, params Expression<Func<TEntity, object>>[] includeProperties);
-    List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, params Expression<Func<TEntity, object>>[] includeProperties);
+    TEntity? Get(
+        Expression<Func<TEntity, bool>> predicate,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
+
+    IPaginate<TEntity> GetList(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
+
+    IPaginate<TEntity> GetListByDynamic(
+        DynamicQuery dynamic,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
+
+    bool Any(Expression<Func<TEntity, bool>>? predicate = null, bool withDeleted = false, bool enableTracking = true);
+    TEntity Add(TEntity entity);
+    ICollection<TEntity> AddRange(ICollection<TEntity> entities);
+    TEntity Update(TEntity entity);
+    ICollection<TEntity> UpdateRange(ICollection<TEntity> entities);
+    TEntity Delete(TEntity entity, bool permanent = false);
+    ICollection<TEntity> DeleteRange(ICollection<TEntity> entity, bool permanent = false);
 }
